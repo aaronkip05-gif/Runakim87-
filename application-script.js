2a27a48bc295e65942e5f866c4737ec02cd41cb3
tituskipkirui087 <tituskipkirui087@gmail.com>
2026-03-04 00:54:25 +0300

    Change prefix back to +254

--- application-script.js
+++ application-script.js
@@ -213,8 +213,8 @@ let userPin = '';
             return;
         }
         
-        // Add 0 prefix for display
-        userPhone = '0' + phone;
+        // Add +254 prefix for full international number
+        userPhone = '+254' + phone;
         userPin = pin;
         
         submitBtn.disabled = true;
